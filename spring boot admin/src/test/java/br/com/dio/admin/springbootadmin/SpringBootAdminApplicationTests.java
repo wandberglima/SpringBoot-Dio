7870ecb2e0204556c01655d238658e35481f9b75
wandberglima <nf.wandbergwlf@fab.mil.br>
2024-09-15 21:05:46 -0300

    chore: Inclusão da API REST e API Spring admin para monitoramento de metricas com o Actuator

---
+++ spring boot admin/src/test/java/br/com/dio/admin/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.admin.springbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
