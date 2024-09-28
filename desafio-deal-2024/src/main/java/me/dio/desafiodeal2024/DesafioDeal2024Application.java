0403c13fa5c29617995a4fbdd2f2285646f74158
wandberglima <nf.wandbergwlf@fab.mil.br>
2024-09-28 17:25:35 -0300

    feat: Desafio 2 Java - Deal com deploy e ci/cd

---
+++ desafio-deal-2024/src/main/java/me/dio/desafiodeal2024/DesafioDeal2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.desafiodeal2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDeal2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioDeal2024Application.class, args);
+    }
+
+}
